578efcd6101d448dcfc3e183d2d58c98347e5d62
Jonas Nogueira de Oliveira <jonasnoliveirabr@gmail.com>
2023-02-04 18:36:39 -0300

    v1.4.1

--- web/src/pages/Dashboard.tsx
+++ web/src/pages/Dashboard.tsx
@@ -27,9 +27,6 @@ export function DashboardPage() {
             </div>
           </div>
         </div>
-        <button>Contas</button>
-        <button>Contas a Pagar</button>
-        <button>Contas a Reeceber</button>
         <div className="relative overflow-x-auto">
           <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
             <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
